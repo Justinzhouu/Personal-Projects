e02db1441b88f6695ae709d2de3f84f9bf8dcf2d
Justinzhou <zhoux20@mcmaster.ca>
2019-04-25 18:20:04 -0400

    adding personal projects

---
+++ QRCodeReader/QRCodeReader/QRCodeViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class QRCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+    
+
+
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ QRCodeReader/QRCodeReader/QRScannerController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class QRScannerController: UIViewController {
+
+    @IBOutlet var messageLabel:UILabel!
+    @IBOutlet var topbar: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+}
